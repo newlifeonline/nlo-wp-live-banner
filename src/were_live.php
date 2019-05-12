591da566fed8a22700a98f59d69cbe4c5a8dc084
Casey Collins <casey.collins@dcsg.com>
2019-05-12 07:53:37 -0400

    adding options

--- src/were_live.php
+++ src/were_live.php
@@ -42,4 +42,51 @@ class LiveBanner {
 }
 
 $wpLiveBanner = new LiveBanner();
+
+function nlo_wl_register_settings()
+{
+    add_option( 'nlo_wl_option_name', 'Were live options');
+    register_setting( 'nlo_wl_options_group', 'nlo_wl_option_name', 'nlo_wl_callback' );
+
+    add_settings_section("section", "Section", null, "demo");
+    add_settings_field("demo-checkbox", "Demo Checkbox", "demo_checkbox_display", "demo", "section");  
+    register_setting("section", "demo-checkbox");
+}
+
+function demo_checkbox_display()
+{
+   ?>
+        <!-- Here we are comparing stored value with 1. Stored value is 1 if user checks the checkbox otherwise empty string. -->
+        <input type="checkbox" name="demo-checkbox" value="1" <?php checked(1, get_option('demo-checkbox'), true); ?> /> 
+   <?php
+}
+add_action( 'admin_init', 'nlo_wl_register_settings' );
+
+function nlo_wl_register_options_page()
+{
+     add_options_page('NLO - Were Live', 'NLO - Were Live', 'manage_options', 'nlo_wl', 'nlo_wl_options_page');
+}
+add_action('admin_menu', 'nlo_wl_register_options_page');
+
+function nlo_wl_options_page()
+{
+?>
+  <div>
+  <?php screen_icon(); ?>
+  <h2>NLO - Were Live</h2>
+  <form method="post" action="options.php">
+  <?php settings_fields( 'nlo_wl_options_group' ); ?>
+  <h3>This is my option</h3>
+  <p>Some text here.</p>
+  <table>
+  <tr valign="top">
+  <th scope="row"><label for="nlo_wl_option_name">Label</label></th>
+  <td><input type="text" id="nlo_wl_option_name" name="nlo_wl_option_name" value="<?php echo get_option('nlo_wl_option_name'); ?>" /></td>
+  </tr>
+  </table>
+  <?php  submit_button(); ?>
+  </form>
+  </div>
+<?php
+}
 ?>
